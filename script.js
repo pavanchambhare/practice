41f64d4cd242f411f1b017161c9013179d53cb27
pavanchambhare <pavanchambhare88@gmail.com>
2022-04-27 21:09:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,180 @@
+// const fullName = "hitesh'chodary"
+// console.log(fullName)
+
+
+// const corseName = "javasript"
+// const islogggedIn = false;
+// console.log(corseName)
+
+// const uid = "jdbjb24f54"
+
+// let fullName = "Pavan Chambhare" 
+// let email = "pavanchambare88@gmail.com"
+// let password = "1545524"
+// let contry = "india"
+
+
+// console.log(
+//     "Full name is" + fullName,
+//     "email is" + email,
+//     "and password  is" + password,
+
+// )
+
+// const sellingPrice = 199;
+// const listingPrice = 800;
+
+
+
+// const discountPercent = (( (sellingPrice - listingPrice)/listingPrice) * 100)
+
+
+//  console.log("discount Percent is" , discountPercent)
+
+//  const shopPrice = 200;
+//  const carPrice = 500;
+ 
+ 
+ 
+//  const disscount = (( (shopPrice - carPrice)/listingPrice) * 100)
+ 
+ 
+//   console.log("discount my " , disscount)
+ 
+// conditional in Javascript
+
+// tempreture = 20;
+
+// if(tempreture > 30) {
+//     console.log("outside is Very Hot")
+// }
+// else {
+//     console.log("outside is Very cold")
+// }
+//logical conditional 
+// const facebook = true;
+// const email = false;
+// const Whatsup = true;
+
+// if (facebook ){
+//     console.log("login suscess")
+// }
+// if (email){
+//     console.log("login suscess")
+// }
+// if (Whatsup ){
+//     console.log("login suscess")
+// }
+// const Vijay = 10;
+
+// if ( Vijay > 50 ){
+//     console.log("login suscess")
+// }
+//     else{
+//         console.log("login fail")
+//     }
+
+//Tarnory oparator
+// let autoentical = false;
+
+
+// if (autoentical){
+//   console.log("show sigout button");
+// }
+// else{
+//     console.log("show sigin button");
+// }
+
+// autoentical ? console.log("logout ") : console.log("login");
+
+// let user = "admin"
+
+// switch (user) {
+//     case "admin":
+//     console.log("you get full name")
+//     break;
+//     case "pavan":
+//     console.log("you not get full name")
+//     break;
+//     case "atom":
+//     console.log("missing somting name")
+//     break;
+//     case "mweo":
+//     console.log("some mistake cheak name")
+//     break;
+
+//     default:
+//     console.log("tril user")
+//     break;
+// }
+
+
+
+// const user = "user";
+
+//   switch (user) {
+//       case "hitesh":
+//           console.log( "hitesh is  My Friend")
+    
+//    case "mohit":
+//     console.log( "mohit is not My Friend")
+//     break;
+
+//  case "manoj":
+//     console.log( "manoj is  My ")
+//     break;
+
+//  case "herry":
+// console.log( "herry is  My Friend")
+// break;
+
+// default:
+// console.log("user")
+//  break;
+// }
+  
+
+// const a = 50;
+// const b = 30;
+// const c = 50;
+
+
+// if (a > b) {
+//     console.log("it is true value")
+// }
+// else{
+//     console.log("it is false value")
+// }
+// console.log("total Is",(( a + b)/c )*100);
+
+//   const user = "mayur";
+
+// switch(user){
+//     case "mayur":
+//         console.log("is my buddy friend");
+
+// }
+// console.log(user)
+
+// bulb = 20;
+  
+ 
+//  if( bulb < 30 ){
+//  console.log("it is true value")
+// }
+// else{
+//     console.log("it is false value")
+// } 
+
+function multable(num){
+
+    for(i=1;  i<=10; i++){
+        document.write('${num} * ${i} = ${num*i}');
+        document.write("<br></br>");
+
+    }
+
+multable(i)
+
+
+}
